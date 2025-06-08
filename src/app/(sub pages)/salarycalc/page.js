103dd201d936b9fd923bd630e4fe0a9a0d3d4299
alaqrasenpai <52831910+alaqrasenpai@users.noreply.github.com>
2025-06-08 12:23:02 +0300

    change title

--- src/app/(sub pages)/salarycalc/page.js
+++ src/app/(sub pages)/salarycalc/page.js
@@ -1,9 +1,22 @@
 import SalaryCalculator from "@/components/SalaryCalculator";
+import Head from "next/head";
 
 export default function Home() {
     return (
-        <main>
-            <SalaryCalculator />
-        </main>
+        <>
+            <Head>
+                <title>Salary Calculator | حاسبة الرواتب والمستحقات</title>
+                <meta name="description" content="أداة لحساب الرواتب والمستحقات للموظفين بناءً على بيانات وزارة المالية" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>
+                <div className="container mx-auto px-4 py-8">
+
+                    <SalaryCalculator />
+                </div>
+            </main>
+        </>
     );
 }
