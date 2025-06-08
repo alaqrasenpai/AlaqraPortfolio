2244571be279aded8be9083035f8a6b263c161cd
alaqrasenpai <52831910+alaqrasenpai@users.noreply.github.com>
2025-06-08 14:13:15 +0300

    google analityx

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import clsx from "clsx";
 import FireFliesBackground from "@/components/FireFliesBackground";
 import Sound from "@/components/Sound";
+import Script from "next/script"; // 👈 استيراد سكربت
 
 const inter = Inter({
   subsets: ["latin"],
@@ -11,18 +12,30 @@ const inter = Inter({
 
 export const metadata = {
   title: {
-    template:
-      "Ahmad Alaqra Portfolio",
-    default:
-      "Ahmad Alaqra Portfolio",
+    template: "Ahmad Alaqra Portfolio",
+    default: "Ahmad Alaqra Portfolio",
   },
-  description:
-    "a personal portfolio developed by ahmad alaqra.",
+  description: "a personal portfolio developed by ahmad alaqra.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* 👇 سكربت Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-9VZ7ZL0NBX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-9VZ7ZL0NBX');
+          `}
+        </Script>
+      </head>
       <body
         className={clsx(
           inter.variable,
